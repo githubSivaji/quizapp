6280a2144ec0a02a00a98506aaf895fdcca62a68
githubSivaji <123107544+githubSivaji@users.noreply.github.com>
2024-08-06 18:17:32 +0530

    sivaji_9

--- src/Components/Quizopen.js
+++ src/Components/Quizopen.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-// import quizData from './quizData';
 import { QuizContext } from './QuizContext';
-import './Quiz.css'
+import './Quiz.css';
+
 const Quizopen = () => {
     const { quizId } = useParams();
     const navigate = useNavigate();
-    const {quizzes}=useContext(QuizContext);
+    const { quizzes } = useContext(QuizContext);
     const quiz = quizzes.find(q => q.id === quizId);
 
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -14,6 +14,16 @@ const Quizopen = () => {
     const [score, setScore] = useState(0);
     const [timeLeft, setTimeLeft] = useState(quiz ? quiz.duration * 60 : 0); // Duration in seconds
     const [quizSubmitted, setQuizSubmitted] = useState(false);
+
+    const calculateScore = () => {
+        let newScore = 0;
+        quiz.questions.forEach((question, index) => {
+            if (selectedAnswers[index] === question.answer) {
+                newScore += 1;
+            }
+        });
+        setScore(newScore);
+    };
 
     useEffect(() => {
         if (timeLeft > 0) {
@@ -26,7 +36,7 @@ const Quizopen = () => {
             calculateScore();
             setQuizSubmitted(true);
         }
-    }, [timeLeft, quizSubmitted]);
+    }, [timeLeft, quizSubmitted, calculateScore]);
 
     const handleAnswerChange = (questionIndex, answer) => {
         setSelectedAnswers({
@@ -59,16 +69,6 @@ const Quizopen = () => {
         setQuizSubmitted(true);
     };
 
-    const calculateScore = () => {
-        let newScore = 0;
-        quiz.questions.forEach((question, index) => {
-            if (selectedAnswers[index] === question.answer) {
-                newScore += 1;
-            }
-        });
-        setScore(newScore);
-    };
-
     if (!quiz) {
         return <div>Quiz not found</div>;
     }
@@ -77,94 +77,74 @@ const Quizopen = () => {
         return (
             <div className='container p-5 text-center' style={{height: "90vh"}}>
                 <h1>Quiz Results</h1>
-                <div className='text-success' style={{fontSize: "100px"}}><i class="fa-solid fa-circle-check"></i></div>
-                {/* <h1>{quiz.title}</h1> */}
-
+                <div className='text-success' style={{fontSize: "100px"}}><i className="fa-solid fa-circle-check"></i></div>
                 <div className='container d-flex justify-content-center gap-5 '>
-                    <div className='score-box p-2' >
+                    <div className='score-box p-2'>
                         <p>YOUR SCORE</p>
-                        <div style = {{ fontSize: "50px"}} className='text-success'>
+                        <div style={{ fontSize: "50px" }} className='text-success'>
                             {score}
                         </div>
                     </div>
                     <div className='score-box p-2'>
-                        <p>correct Questions : <h1>{score}</h1> </p>
-                        <p>Incorrect Questions: {quiz.questions.length-score} </p>
-
+                        <p>Correct Questions: <h1>{score}</h1></p>
+                        <p>Incorrect Questions: {quiz.questions.length - score}</p>
                     </div>
                 </div>
-                <div className='btn btn-primary mt-5' onClick={()=>{
-                    navigate('/')
-                }}>Back to Home</div>
+                <div className='btn btn-primary mt-5' onClick={() => navigate('/')}>Back to Home</div>
             </div>
         );
     }
 
     return (
         <div className='container p-3 main'>
-    
-            <h3 style={{textAlign:"center"}}>{quiz.title}</h3><br></br>
-            
-            <div className='container  next'>
-                    <div className='container d-flex gap-5 justify-content-end'>
+            <h3 style={{ textAlign: "center" }}>{quiz.title}</h3><br />
+            <div className='container next'>
+                <div className='container d-flex gap-5 justify-content-end'>
                     <h6>Time left: {Math.floor(timeLeft / 60)}:{String(timeLeft % 60).padStart(2, '0')}</h6>
-                    <button  className='btn btn-danger' onClick={handleFinishTest}>
-                        Finish Test
-                    </button>
+                    <button className='btn btn-danger' onClick={handleFinishTest}>Finish Test</button>
+                </div>
 
-            </div>
-
-            {/* navigation */}
-            <div className='text-center mt-3 overflow-auto'>
-            {quiz.questions.map((_, index) => (
-                <button
-                    key={index}
-                    className={index === currentQuestionIndex ? 'active navigate-btns mx-2 bg-primary' : 'mx-2 navigate-btns '}
-                    onClick={() => handleQuestionClick(index)}
-                >
-                    {index + 1}
-                </ button>
-            ))}
-            </div>
-                
+                <div className='text-center mt-3 overflow-auto'>
+                    {quiz.questions.map((_, index) => (
+                        <button
+                            key={index}
+                            className={index === currentQuestionIndex ? 'active navigate-btns mx-2 bg-primary' : 'mx-2 navigate-btns'}
+                            onClick={() => handleQuestionClick(index)}
+                        >
+                            {index + 1}
+                        </button>
+                    ))}
+                </div>
             </div>
 
             <div className='container next mt-5'>
-            <div>
-                <h5>{currentQuestionIndex + 1}: {quiz.questions[currentQuestionIndex].question}</h5>
-                {quiz.questions[currentQuestionIndex].options.map((option, i) => (
-                    <div key={i} className='container w-100 option'>
-                        
-                        <input
-                            type="radio"
-                            id={`question${currentQuestionIndex}_option${i}`}
-                            name={`question${currentQuestionIndex}`}
-                            value={option}
-                            checked={selectedAnswers[currentQuestionIndex] === option}
-                            onChange={() => handleAnswerChange(currentQuestionIndex, option)}
-                        />
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                        <label htmlFor={`question${currentQuestionIndex}_option${i}`}>{option}</label>
-                        
-                    </div>
-                    
-                
-                ))}
-            </div>
-            <div className='d-flex justify-content-between'>
-                <button className = 'btn btn-outline-primary mx-2' onClick={handlePreviousQuestion} disabled={currentQuestionIndex === 0}>
-                    Previous
-                </button>
-                {currentQuestionIndex < quiz.questions.length - 1 ? (
-                    <button  className = 'btn btn-outline-primary' onClick={handleNextQuestion}>
-                        Next
+                <div>
+                    <h5>{currentQuestionIndex + 1}: {quiz.questions[currentQuestionIndex].question}</h5>
+                    {quiz.questions[currentQuestionIndex].options.map((option, i) => (
+                        <div key={i} className='container w-100 option'>
+                            <input
+                                type="radio"
+                                id={`question${currentQuestionIndex}_option${i}`}
+                                name={`question${currentQuestionIndex}`}
+                                value={option}
+                                checked={selectedAnswers[currentQuestionIndex] === option}
+                                onChange={() => handleAnswerChange(currentQuestionIndex, option)}
+                            />
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <label htmlFor={`question${currentQuestionIndex}_option${i}`}>{option}</label>
+                        </div>
+                    ))}
+                </div>
+                <div className='d-flex justify-content-between'>
+                    <button className='btn btn-outline-primary mx-2' onClick={handlePreviousQuestion} disabled={currentQuestionIndex === 0}>
+                        Previous
                     </button>
-                ) : (
-                    <button className = 'btn btn-success' onClick={handleFinishTest}>
-                        Submit
-                    </button>
-                )}
-            </div>
+                    {currentQuestionIndex < quiz.questions.length - 1 ? (
+                        <button className='btn btn-outline-primary' onClick={handleNextQuestion}>Next</button>
+                    ) : (
+                        <button className='btn btn-success' onClick={handleFinishTest}>Submit</button>
+                    )}
+                </div>
             </div>
         </div>
     );
